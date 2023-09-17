387e511fe7059e92d5bbd6cffdc5a168ab06b245
LuckyBelieve <luckybelieve10@gmail.com>
2023-09-17 12:47:46 +0200

    jobHub project setup

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+const Home = ()=>{
+    return <View>
+        <Text>
+            this is the home page
+        </Text>
+    </View>
+}
+export default Home;
